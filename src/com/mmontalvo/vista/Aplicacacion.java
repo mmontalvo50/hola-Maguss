eba75e517740922b433c6ec98918ec46c627b438
mmontalvo50 <magus2103@gmail.com>
2018-10-24 11:50:50 -0500

    Este es111

---
+++ src/com/mmontalvo/vista/Aplicacacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mmontalvo.vista;
+
+/**
+ *
+ * @author PC6-LAB2
+ */
+public class Aplicacacion {
+    public static void main(String[] args) {
+        System.out.println("Hola magus");
+    }
+}
